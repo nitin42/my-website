25349ac2677cf6cdf1fd692d96f2459dfe3ffbd8
Nitin <tulswani19@gmail.com>
2018-08-10 15:08:15 +0530

    refactored codebase

--- src/components/Highlights.js
+++ src/components/Highlights.js
@@ -1,40 +1,60 @@
-import React from 'react';
-import Link from 'gatsby-link';
+import React from 'react'
+import Link from 'gatsby-link'
 
 const highlights = [
-	{
-		title: 'An interactive, explorable explanation about spring physics animation',
-		date: ' - 31 May, 2018',
-		link: '/posts/spring',
-	},
-	{
-		title: 'Elements of Physics - An interactive physics simulation to learn different elements of physics',
-		date: ' - 27 June, 2018',
-		link: 'https://elements-of-physics.surge.sh',
-	},
-	{ title: 'Creating microworlds', date: ' - 17 July, 2018', link: '/posts/microworlds' },
-];
+  {
+    title:
+      'An interactive, explorable explanation about spring physics animation',
+    date: ' - 31 May, 2018',
+    link: '/posts/spring',
+  },
+  {
+    title:
+      'Elements of Physics - An interactive physics simulation to learn different elements of physics',
+    date: ' - 27 June, 2018',
+    link: 'https://elements-of-physics.surge.sh',
+    useLink: true,
+  },
+  {
+    title: 'Creating microworlds',
+    date: ' - 17 July, 2018',
+    link: '/posts/microworlds',
+  },
+]
 
 export default () => (
-	<React.Fragment>
-		<h1
-			style={{
-				fontSize: '1.8em',
-				borderLeft: '8px solid black',
-				paddingLeft: '10px',
-			}}
-		>
-			Highlights
-		</h1>
-		<ul>
-			{highlights.map(highlight => (
-				<li>
-					<Link to={highlight.link}>
-						<span>{highlight.title}</span>
-						<span id="date">{highlight.date}</span>
-					</Link>
-				</li>
-			))}
-		</ul>
-	</React.Fragment>
-);
+  <React.Fragment>
+    <h1
+      style={{
+        fontSize: '1.8em',
+        borderLeft: '8px solid black',
+        paddingLeft: '10px',
+      }}
+    >
+      Highlights
+    </h1>
+    <ul>
+      {highlights.map(highlight => {
+        if (highlight.useLink) {
+          return (
+            <li>
+              <a href={highlight.link} target="_blank">
+                <span>{highlight.title}</span>
+                <span id="date">{highlight.date}</span>
+              </a>
+            </li>
+          )
+        } else {
+          return (
+            <li>
+              <Link to={highlight.link}>
+                <span>{highlight.title}</span>
+                <span id="date">{highlight.date}</span>
+              </Link>
+            </li>
+          )
+        }
+      })}
+    </ul>
+  </React.Fragment>
+)
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,54 +1,58 @@
-import React from 'react';
+import React from 'react'
 
 const projects = [
-	{
-		name: 'Redocx',
-		source: 'https://github.com/nitin42/redocx',
-		description: '	- A custom renderer to render React components to word documents',
-	},
-	{
-		name: 'React Performance Devtool',
-		source: 'https://github.com/nitin42/react-perf-devtool',
-		description: ' - A chrome extension devtool to inspect the performance of a React application',
-	},
-	{
-		name: 'React Image Processing',
-		source: 'https://github.com/nitin42/react-imgpro',
-		description:
-			'	- An image processing component for React which uses worker thread for processing and manipulating an image with different filters.',
-	},
-	{
-		name: 'Animated Timeline',
-		source: 'https://github.com/nitin42/Animated-Timeline',
-		description: '	- Set of APIs and tools to create timeline and playback based animations',
-	},
-	{
-		name: 'Terminal in React',
-		source: 'https://github.com/nitin42/terminal-in-react',
-		description: '	- A React component that renders a terminal on a web page with different options and plugins',
-	},
-];
+  {
+    name: 'Redocx',
+    source: 'https://github.com/nitin42/redocx',
+    description:
+      '	- A custom renderer to render React components to word documents',
+  },
+  {
+    name: 'React Performance Devtool',
+    source: 'https://github.com/nitin42/react-perf-devtool',
+    description:
+      ' - A chrome extension devtool to inspect the performance of a React application',
+  },
+  {
+    name: 'React Image Processing',
+    source: 'https://github.com/nitin42/react-imgpro',
+    description:
+      '	- An image processing component for React which uses worker thread for processing and manipulating an image with different filters.',
+  },
+  {
+    name: 'Animated Timeline',
+    source: 'https://github.com/nitin42/Animated-Timeline',
+    description:
+      '	- Set of APIs and tools to create timeline and playback based animations',
+  },
+  {
+    name: 'Terminal in React',
+    source: 'https://github.com/nitin42/terminal-in-react',
+    description:
+      '	- A React component that renders a terminal on a web page with different options and plugins',
+  },
+]
 
 export default () => (
-	<React.Fragment>
-		<h1
-			style={{
-				marginTop: '1.5em',
-				borderLeft: '8px solid black',
-				paddingLeft: '10px',
-			}}
-		>
-			Some of my projects
-		</h1>
-		<ul id="projects">
-			{projects.map(project => (
-				<li>
-					<a href={project.source} target="_blank">
-						{project.name}
-					</a>
-					{project.description}
-				</li>
-			))}
-		</ul>
-	</React.Fragment>
-);
+  <React.Fragment>
+    <h1
+      style={{
+        marginTop: '1.5em',
+        borderLeft: '8px solid black',
+        paddingLeft: '10px',
+      }}
+    >
+      Some of my projects
+    </h1>
+    <ul id="projects">
+      {projects.map(project => (
+        <li>
+          <a href={project.source} target="_blank">
+            {project.name}
+          </a>
+          {project.description}
+        </li>
+      ))}
+    </ul>
+  </React.Fragment>
+)
