3e798e73371fbfd7a2cd692074166a95ee8d1334
Nitin <tulswani19@gmail.com>
2019-03-13 16:55:00 +0000

    remove unused imports

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,14 +5,12 @@ import Projects from './Projects'
 import Other from './Other'
 import Header from './Header'
 
-import { WindowSubscriptionsProvider } from './WindowSubscription'
-
 export default () => (
-  <div className="home">
-    <div className="introduction">
-      <Introduction />
-      <Projects />
-      <Other />
-    </div>
-  </div>
+	<div className="home">
+		<div className="introduction">
+			<Introduction />
+			<Projects />
+			<Other />
+		</div>
+	</div>
 )
